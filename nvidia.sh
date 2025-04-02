c0fee4a784065da4bebb778edf97d2ae70005774
Jackblow33 <jackblow33@hotmail.com>
2025-04-02 19:15:44 -0400

    Create nvidia.sh

---
+++ nvidia.sh
@@ -0,0 +1,11 @@
+
+
+
+# Nvidia drivers install
+
+#Detect VGA & kernel     # depending on kernel version diff method to install..
+lspci -v | grep -A10 VGA
+uname -r
+
+#Installing dkms packages
+ 
